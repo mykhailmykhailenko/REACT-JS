58de1d182f804cf9225893ed2435980168f81f78
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-09 19:22:52 +0200

    Function and Class components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import OurComponent from './OurComponent';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // React.createElement(OurComponent, {today: 'tuesday'})
-  <div>
-     <OurComponent today="monday"/>
-     <OurComponent today="tuesday"/>
-     <OurComponent today="wednesday"/>
-  </div>
-
-);
+       <OurComponent today="monday"/>
+       );
 
