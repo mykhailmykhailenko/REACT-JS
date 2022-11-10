9814324f825e914c660fdd511ff6d8bb4ae4fd55
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-10 07:35:22 +0200

    React-component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
-// const h1 = React.createElement('h1', {}, 'Hello, World');
+ const component = React.createElement(Section);
 
-
-// const div = React.createElement('div',{}, h1);
-const div = (<section>
-                     <div>
-                            <h1>Hello, world</h1>
-                     </div>
-                     <article>
-                            <p>Lorem</p>
-                     </article>
-              </section>
-              );
 
 const rootElement = document.querySelector('#root');
 const root = ReactDOM.createRoot(rootElement);
-root.render(div);
+root.render(component);
 
+function Section(props){
 
-console.log(root);
+       const username = 'John';
+       const today = new Date();
+
+       return (
+              <section>
+                     <div>
+                            <h1>Hello, {username}</h1>
+                     </div>
+                     <article>
+                            <p>{today.toDateString()}</p>
+                            <button>{vipText}</button>
+                     </article>
+              </section>
+       )
+}
