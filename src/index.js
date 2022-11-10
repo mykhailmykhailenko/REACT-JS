327600f947e5f3fe619b294883db9c3412775b9d
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-10 07:34:34 +0200

    React. again basics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client';
-import OurComponent from './OurComponent';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-       <OurComponent today="monday"/>
-       );
+// const h1 = React.createElement('h1', {}, 'Hello, World');
 
+
+// const div = React.createElement('div',{}, h1);
+const div = (<section>
+                     <div>
+                            <h1>Hello, world</h1>
+                     </div>
+                     <article>
+                            <p>Lorem</p>
+                     </article>
+              </section>
+              );
+
+const rootElement = document.querySelector('#root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(div);
+
+
+console.log(root);
