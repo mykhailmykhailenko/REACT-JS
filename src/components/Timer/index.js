548c4532c0c664c5d0687a99584ec15f3647271b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-14 13:01:06 +0200

    date-fns timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,45 +1,45 @@
 import React, { Component } from 'react';
+import {format, addSeconds} from 'date-fns';
 
 class Timer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            time: new Date(0,0,0,0,0,0)
         }
-        this.intervalId = null
+        this.intervalId = null;
+    }
 
-   }
-   
-   start = () => {
-        this.intervalId = setInterval(()=> {
-            const {count} = this.state;
+    start = () => {
+       this.intervalId = setInterval(()=>{
+            const {time} = this.state;
+        const newdate = addSeconds(time, 1);
             this.setState({
-                count: count +1
+                time: newdate
             })
-        }, 1000)
-   }
 
-   componentDidMount () {
-        this.start();
-   }
+        }, 1000);
+    }
+    componentDidMount(){
+ //       this.start();
+    }
 
-   componentWillUnmount () {
+    componentWillUnmount(){
         this.stop();
-   }
+    }
+    stop = () => {
+        clearInterval(this.intervalId);
+    }
 
-   stop = () => {
-    clearInterval(this.intervalId)
-   }
-   
     render() {
+        const {time} = this.state;
         return (
             <div>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.stop}>Stop</button>
+               <h1>{format(time, 'HH:mm:ss')}</h1> 
+               <button onClick={this.stop}>Stop</button>
+               <button onClick={this.start}>Start</button>
             </div>
         );
     }
 }
-
 export default Timer;
- 
