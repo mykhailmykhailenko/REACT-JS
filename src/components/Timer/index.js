378a1f6ccfc7e959c9b1b54c16e2687a89b2331a
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-15 13:53:08 +0200

    stopwatch task resolve

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import {format, addSeconds} from 'date-fns';
-
 class Timer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            time: new Date(0,0,0,0,0,0)
+            time: new Date(0,0,0,0,0,0),
+            isRunning: false
         }
         this.intervalId = null;
     }
-
     start = () => {
        this.intervalId = setInterval(()=>{
             const {time} = this.state;
         const newdate = addSeconds(time, 1);
             this.setState({
-                time: newdate
+                time: newdate,
+                isRunning: true
             })
 
         }, 1000);
@@ -23,21 +23,39 @@ class Timer extends Component {
     componentDidMount(){
  //       this.start();
     }
-
     componentWillUnmount(){
         this.stop();
     }
+
     stop = () => {
         clearInterval(this.intervalId);
+        this.setState({
+            isRunning: false
+        })
+    }
+
+    reset = () => {
+        this.setState({
+            time: new Date(0,0,0,0,0,0)
+        })
+    }
+
+    clickHandler = () => {
+        const {isRunning} = this.state;
+        if(isRunning) {
+            this.stop();
+        } else {
+            this.start();
+        }
     }
 
     render() {
-        const {time} = this.state;
+        const {time, isRunning} = this.state;
         return (
             <div>
                <h1>{format(time, 'HH:mm:ss')}</h1> 
-               <button onClick={this.stop}>Stop</button>
-               <button onClick={this.start}>Start</button>
+               <button onClick={this.clickHandler}>{ isRunning ? 'Stop' : 'Start'}</button>
+               <button onClick={this.reset}>Clear</button>
             </div>
         );
     }
