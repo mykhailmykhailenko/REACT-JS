ed2108778a904cb5ad397dbcd6ac8eb384e56369
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-27 22:41:56 +0200

    clipboard methods

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
-
-
-
 class App extends React.Component {
-
-
-
+  
     render () {
 
         return (
             <>
+            <p>lorem ipsum dolor sit amet</p>
             <button onClick={()=>{
-               window.location.assign('https://rozetka.com.ua/');
-            }}>Assing</button>
-            <button onClick={()=>{
-            window.location.reload();
-            }}>Reload</button>
-            <button onClick={()=>{
-             window.location.replace('https://wiki.postgresql.org/wiki/Main_Page');
-            }}>Replace</button>
+                window.navigator.clipboard.writeText('ANOTHER TEXT')
+                .then((data) => {
+                    console.log(data)
+                })
+            }}>Copy it!</button>
+
+
+                <button onClick={()=>{
+                window.navigator.clipboard.readText()
+                .then((data) => {
+                    console.log(data)
+                })
+            }}>Read the buffer!</button>
             </>
         )
     }
