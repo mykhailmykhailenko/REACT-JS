3416741546e24908812b396c1824234add6c4965
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-06 08:08:15 +0200

    Field

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -1,28 +1,26 @@
 import React from "react";
 import styles from './SignUp.module.css';
 import {SCHEMA } from '../../schemas';
-import {Formik, Form, Field} from 'formik';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
 
 function SignUpForm (props) {
-
 
    const formikSubmit = (data, formikBag) => {
         console.log(data, formikBag);
         ////// TODO: send data to server
         formikBag.resetForm();
     }
-
         const initialValues = {
             firstName: '',
             lastName: '',
             email: '',
             pass: ''
         };
-
         return (
             <Formik 
             initialValues={initialValues}
             onSubmit={formikSubmit}
+            validationSchema={SCHEMA}
             >
            {(formikProps) => {
             return (
@@ -31,24 +29,25 @@ function SignUpForm (props) {
                     name="firstName"
                     placeholder="firstName"
                     />
+                    <ErrorMessage name="firstName" component="p"/>
                     <Field 
                     name="lastName"
                     placeholder="lastName"
                     />
+                    <ErrorMessage name="lastName" />
                     <Field 
                     name="email"
                     placeholder="email"
                     />
-                     <Field 
-                    name="pass"
-                    placeholder="pass"
-                    />
-                    <button>Send</button>
+                    <ErrorMessage name="email" />
+                     <Field name="pass" 
+                        placeholder="email"/>
+                        <ErrorMessage name="pass" />
+                    <button type="submit">Send</button>
                 </Form>
             )
            }}
             </Formik>
         )
 }
-
 export default SignUpForm;
