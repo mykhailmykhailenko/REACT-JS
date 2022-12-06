099a2d465fd2ba6d69d65c12ec904430d7606a74
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-06 12:58:47 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from "react";
-import Header from "./components/Header";
-import Tree from "./components/Tree";
-import UserContext from "./contexts/UserContext";
-import ThemeContext from './contexts/ThemeContext';
-import CONSTANTS from './constants';
-import SignUpForm from "./components/SignUpForm";
-const {THEMES} = CONSTANTS;
+import React, {useState} from "react";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+
+function App(props) {
+     const [count, setCount] = useState(0);
+
+    const increment = () => {
+       setCount(count+1);
     }
 
-
-
-    render () {
-        return (
-          <SignUpForm />
-        )
-    }
+    return (
+        <>
+        <h2>{count}</h2>
+        <button onClick={increment}>+</button>
+        </>
+    )
 }
+
+
 export default App;
+
+
+
 /*
-Контекст має три умови
-1. Створити контекст
-2. Огорнути дерево компонент у компонент Provider, передати значення у проп value
-3. Підписати конкретні компоненти, яким необхідно ось це значення, на оновлення контексту (за допомогою Consumer)
+Stateless - той, хто не має стану
+Statefull - той, хто має стан
+function App(props){} -> React.createElement(...);
+class App extend React.Component{} --> App.render() --> React.createElement();
 */
