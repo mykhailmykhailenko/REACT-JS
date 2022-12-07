b45bba0df3875f92f8c58c3ef414868c982fc8b4
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-07 16:20:48 +0200

    callback setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, {useState} from "react";
 import MouseTracker from "./components/MouseTracker";
 import WindowSizer from "./components/WindowSizer";
-import Timer from "./components/Timer"
-
 function App(props) {
    const [isVisible, setVisible] = useState(true);
 
     return (
-        // <> {isVisible && <MouseTracker />}</>
-        <Timer />
+         <> {isVisible && <MouseTracker />}</>
+
     )
 }
 
--- src/components/MouseTracker/index.js
+++ src/components/MouseTracker/index.js
@@ -1,39 +1,34 @@
 import React, {useEffect, useState} from 'react';
 
-const MouseTracker = () => {
-    const [coordinates, setCoordinates] = useState( {x: 0,
-                                            y: 0} );
+const Counter = () => {
+
     const [count, setCount] = useState(0);
 
     useEffect(()=>{
-        console.log('ефект навісили')
-        document.body.addEventListener('mousemove', handleMouseMove);
+        const handleClick = () => {
+            setCount((count) => count+1);
+        }
+
+        window.addEventListener('click', handleClick);
         return () => {
-            console.log('ефект почистили');
-            document.body.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('click', handleClick);
         }
     },[]);
 
-    const handleMouseMove = ({clientX, clientY}) => {
-        setCoordinates({
-            x: clientX,
-            y: clientY
-        })
-    }
-    const handleClick = () => {
-        setCount(count+1)
-    }
-    console.log('render');
+
+
     return (
         <div>
-           <p>X: {coordinates.x}</p> 
-           <p>Y: {coordinates.y}</p>
            <p>Counter: {count}</p>
-           <button onClick={handleClick}>+</button>
+
         </div>
     );
 }
-export default MouseTracker;
+
+export default Counter;
+
+
+
 /*
 this.setState - класова функція зі зміни стану
 const prevState = {
